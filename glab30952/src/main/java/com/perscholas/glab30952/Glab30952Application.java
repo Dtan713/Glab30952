c72d5575b4a11382f94bd4f5f7bfa65d3496cefd
Dtan713 <s.donaldtan@gmail.com>
2024-08-14 14:08:57 -0400

    glab30952

---
+++ glab30952/src/main/java/com/perscholas/glab30952/Glab30952Application.java
@@ -0,0 +1,13 @@
+package com.perscholas.glab30952;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab30952Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab30952Application.class, args);
+	}
+
+}
---
+++ glab30952/src/test/java/com/perscholas/glab30952/Glab30952ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.glab30952;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30952ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
